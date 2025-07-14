ee8caee16aa57016b4b7c1a88ee61372898ffc4b
Brandon <badamsva@gmail.com>
2025-07-14 15:59:39 -0400

    refactor: change how templates get rendered and the structure of them

--- internal/templates/base_templ.go
+++ internal/templates/base_templ.go
@@ -8,7 +8,7 @@ package templates
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Base(title string) templ.Component {
+func Base(title string, content templ.Component) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -42,28 +42,28 @@ func Base(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</title><script src=\"https://unpkg.com/htmx.org@2.0.4/dist/htmx.js\" integrity=\"sha384-oeUn82QNXPuVkGCkcrInrS1twIxKhkZiFfr2TdiuObZ3n3yIeMiqcRzkIcguaof1\" crossorigin=\"anonymous\"></script><script src=\"https://cdn.tailwindcss.com\"></script></head><body><nav><a href=\"/\">Home</a></nav><h1>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</title><script src=\"https://unpkg.com/htmx.org@2.0.4/dist/htmx.js\" integrity=\"sha384-oeUn82QNXPuVkGCkcrInrS1twIxKhkZiFfr2TdiuObZ3n3yIeMiqcRzkIcguaof1\" crossorigin=\"anonymous\"></script><script src=\"https://cdn.tailwindcss.com\"></script></head><body><!-- Nav bar is shared between pages --><nav class=\"bg-gray-800 text-white p-4\"><div class=\"container mx-auto\"><a href=\"/\" class=\"text-white hover:text-gray-300\">Home</a></div></nav><main><h1 class=\"text-3xl font-bold text-center my-6\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(title)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/base.templ`, Line: 18, Col: 14}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/base.templ`, Line: 21, Col: 59}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</h1>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</h1><!-- The content component gets rendered here -->")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = content.Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</main></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"arcade-website/internal/database"
 	"arcade-website/internal/handlers"
 	"arcade-website/internal/templates"
+	"arcade-website/internal/templates/submit"
+
+	"github.com/a-h/templ"
 
 	"fmt"
 	"log"
 
 	"github.com/labstack/echo/v4"
 )
+
+func renderTempl(c echo.Context, component templ.Component) error {
+	c.Response().Header().Set("Content-Type", "text/html")
+	c.Response().WriteHeader(c.Response().Status)
+	return component.Render(c.Request().Context(), c.Response().Writer)
+}
 
 func main() {
 	db, err := database.DatabaseConnection()
@@ -25,21 +34,24 @@ func main() {
 	e.GET("/submit/:game_id", func(c echo.Context) error {
 		gameID := c.Param("game_id")
 		print(gameID)
-		component := templates.ScoreSubmission(gameID)
-		return component.Render(c.Request().Context(), c.Response().Writer)
+		content := submit.ScoreSubmission(gameID)
+
+		page := templates.Base("Score Submission", content)
+		return renderTempl(c, page)
 	})
 
-	e.POST("/upload", handlers.UploadScore(db))
-
+	// APIs
+	e.POST("/api/upload", handlers.PostScore(db))
 	// e.GET("/search/:option", database.SearchUser(db))
-
-	e.GET("/set-username", func(c echo.Context) error {
+	e.GET("/api/set-username", func(c echo.Context) error {
 		username := c.QueryParam("username")
 		if username == "" {
 			fmt.Println("Is initials")
 		}
-		component := templates.UsernameInput(username)
-		return component.Render(c.Request().Context(), c.Response().Writer)
+		component := submit.UsernameInput(username)
+		return renderTempl(c, component)
+
+		// return component.Render(c.Request().Context(), c.Response().Writer)
 	})
 
 	fmt.Println("Started Server on :6969")
