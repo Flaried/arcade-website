41d8658221b2fa8f22d5f38bd79a053a42f7038d
Brandon <badamsva@gmail.com>
2025-07-15 17:55:02 -0400

    bug: don't save the file if the database errors

--- internal/handlers/score.go
+++ internal/handlers/score.go
@@ -3,10 +3,12 @@ package handlers
 import (
 	"arcade-website/internal/model"
 	"database/sql"
+	"errors"
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"log"
 	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 // TODO: return errors and html more gracefully
@@ -18,15 +20,14 @@ func PostScore(db *sql.DB) echo.HandlerFunc {
 		// c.Request().Body = http.MaxBytesReader(c.Response().Writer, c.Request().Body, 30*1024*1024)
 		//
 		Submission.Score = c.FormValue("score")
-
-		fmt.Println(c.FormValue)
 		Submission.Username = c.FormValue("username")
 		Submission.Initials = c.FormValue("initials")
 		Submission.GameID = c.FormValue("game_id")
 
 		//TODO: EXIT/sanatize all form values
 
-		file, err := c.FormFile("photo")
+		/// User inputed bytes.
+		userFile, err := c.FormFile("photo")
 		if err != nil {
 			log.Println(err.Error())
 			// TODO: RETURN ERROR
@@ -34,7 +35,7 @@ func PostScore(db *sql.DB) echo.HandlerFunc {
 			return c.HTML(200, `<div class="text-red-500">Missing file upload</div>`)
 		}
 
-		imageBytes, err := validatePicture(file)
+		imageBytes, err := validatePicture(userFile)
 		if err != nil {
 			c.Response().Header().Set("HX-Reswap", "innerHTML")
 			fmt.Println(err.Error())
@@ -42,28 +43,46 @@ func PostScore(db *sql.DB) echo.HandlerFunc {
 		}
 
 		savedPath := fmt.Sprintf("scores/%s_%s_%s_%s.jpeg", Submission.GameID, Submission.Username, Submission.Score, "pending")
-		f, err := os.Create(savedPath)
-		if err != nil {
-			log.Println(err.Error())
-			c.Response().Header().Set("HX-Reswap", "innerHTML")
-			return c.HTML(200, `<div class="text-red-500">Error Downloading file</div>`)
-		}
-		defer f.Close()
 
-		_, err = f.Write(imageBytes)
+		err = savePicture(imageBytes, savedPath)
 		if err != nil {
-			log.Println(err.Error())
-			c.Response().Header().Set("HX-Reswap", "innerHTML")
-			return c.HTML(200, `<div class="text-red-500">Error Creating Image file</div>`)
+			return c.HTML(200, err.Error())
 		}
 
 		fmt.Println("Submission", Submission, Submission.Username)
 		_, err = db.Exec("INSERT INTO users (username, initials) VALUES ($1, $2)", Submission.Username, Submission.Initials)
 		if err != nil {
+			err = os.Remove(savedPath)
+			if err != nil {
+				fmt.Printf("Couldn't remove %s\n", savedPath)
+			}
+
 			c.Response().Header().Set("HX-Reswap", "innerHTML")
 			return c.HTML(200, `<div class="text-red-500">Error Writing to Database File</div>`)
 		}
 
 		return c.HTML(200, `<div class="text-green-500">Sucessfully Published Score</div>`)
 	}
+}
+
+func savePicture(imageBytes []byte, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Println(err.Error())
+		return errors.New(`<div class="text-red-500">Error Downloading file</div>`)
+	}
+
+	_, err = file.Write(imageBytes)
+	if err != nil {
+		log.Println(err.Error())
+		// c.Response().Header().Set("HX-Reswap", "innerHTML")
+		return errors.New(`<div class="text-red-500">Error Creating Image file</div>`)
+	}
+
+	err = file.Close()
+	if err != nil {
+		return errors.New(`<div class="text-red-500">Error closing file</div>`)
+	}
+
+	return nil
 }
